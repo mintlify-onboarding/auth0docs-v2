475baf0c2929e494bc7e54a2d9ad652ec813b97c
Pavlo Korobchuk <pavelkorobchuk@gmail.com>
2025-09-16 14:53:03 +0200

    fix: reverted user-authentication.jsx

--- auth4genai/snippets/get-started/prerequisites/user-authentication.jsx
+++ auth4genai/snippets/get-started/prerequisites/user-authentication.jsx
@@ -4,20 +4,13 @@ export const Prerequisites = ({
   callbackUrl = "http://localhost:3000/auth/callback",
   logoutUrl = "http://localhost:3000",
 }) => {
-  // Build steps array dynamically based on conditions
-  const steps = [];
-
-  steps.push(
-    ...AccountAndAppSteps({ callbackUrl, logoutUrl })
-  );
-
   return (
     <>
       <Heading level={3} id="prerequisites">
         Prerequisites
       </Heading>
       Before getting started, make sure you have completed the following steps:
-      <Steps>{steps}</Steps>
+      <Steps>{AccountAndAppSteps({ callbackUrl, logoutUrl })}</Steps>
     </>
   );
 };
