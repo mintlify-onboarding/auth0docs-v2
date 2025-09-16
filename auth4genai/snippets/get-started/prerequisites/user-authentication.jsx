05236f0d765091b2c9626dbd6b1fa81e47061f50
Pavlo Korobchuk <pavelkorobchuk@gmail.com>
2025-09-16 14:51:23 +0200

    fix: moved creating custom api to the langgraph examples

--- auth4genai/snippets/get-started/prerequisites/user-authentication.jsx
+++ auth4genai/snippets/get-started/prerequisites/user-authentication.jsx
@@ -3,7 +3,6 @@ import { AccountAndAppSteps } from "/snippets/get-started/prerequisites/account-
 export const Prerequisites = ({
   callbackUrl = "http://localhost:3000/auth/callback",
   logoutUrl = "http://localhost:3000",
-  createResourceServerClientStep = undefined,
 }) => {
   // Build steps array dynamically based on conditions
   const steps = [];
@@ -11,50 +10,6 @@ export const Prerequisites = ({
   steps.push(
     ...AccountAndAppSteps({ callbackUrl, logoutUrl })
   );
-
-  if (createResourceServerClientStep) {
-    steps.push(
-      <Step key="resource-server" title="Create a Custom API Client">
-        The Custom API Client allows your API server to perform token
-        exchanges using{" "}
-        <strong>
-          <i>access tokens</i>
-        </strong>{" "}
-        instead of{" "}
-        <strong>
-          <i>refresh tokens</i>
-        </strong>
-        . This client enables Token Vault to exchange an access token for an
-        external API access token (e.g., Google Calendar API).
-        <br />
-        This will be used by the Langgraph API server
-        (@langchain/langgraph-cli or Langgraph Platform) when executing tools that require third-party access.
-        <br />
-        <ul>
-              <li>
-                Navigate to{" "}
-                <strong>
-                  Applications &gt; APIs
-                </strong>
-              </li>
-              <li>
-                Click the{" "}
-                <strong>Create API</strong> button to create a new Custom API.
-              </li>
-              <li>
-                Go to the Custom API you created and click the <strong>Add Application</strong> button in the right top corner.
-              </li>
-              <li>
-                After that click the <strong>Configure Application</strong> button in the right top corner.
-              </li>
-          <li>
-            Note down the <code>client id</code> and <code>client secret</code>{" "}
-           for your environment variables.
-          </li>
-        </ul>
-      </Step>
-    );
-  }
 
   return (
     <>
